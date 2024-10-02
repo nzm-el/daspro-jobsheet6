f92591a867f7901fc113a70b388ff7371de722f7
nzm-el <nizamel1402@gmail.com>
2024-10-02 11:39:15 +0700

    Percobaan 1

---
+++ Pemilihan2Percobaan118.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan118 {
+    public static void main(String[] args) {
+        Scanner sc18 = new Scanner(System.in);
+        int tahun;
+        System.out.println("Masukan Tahun : ");
+        tahun = sc18.nextInt();
+
+        if ((tahun % 4) == 0){
+            if ((tahun % 100) != 0)
+                System.out.println("Tahun Kabisat");
+        } else 
+            System.out.println("Bukan Tahun Kabisat");
+    }
+}
