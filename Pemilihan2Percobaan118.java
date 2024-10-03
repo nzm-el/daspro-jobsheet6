c5731024101e131e147f3a980a7f54e3e564c125
Nizam El <nizamel1402@gmail.com>
2024-10-03 08:08:37 +0700

    Percobaan 1

--- Pemilihan2Percobaan118.java
+++ Pemilihan2Percobaan118.java
@@ -7,8 +7,10 @@ public class Pemilihan2Percobaan118 {
         tahun = sc18.nextInt();
 
         if ((tahun % 4) == 0){
-            if ((tahun % 100) != 0)
+            if ((tahun % 100) != 0) {
                 System.out.println("Tahun Kabisat");
+            } else
+            System.out.println("BUkan Tahun Kabisat");
         } else 
             System.out.println("Bukan Tahun Kabisat");
     }
